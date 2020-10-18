39ce7d09dfb6904f39b3192bdc231023635b6900
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-18 14:38:34 +0200

    Adding repositories

---
+++ src/main/java/edu/intech/intflix/model/SeasonRepository.java
@@ -0,0 +1,6 @@
+package edu.intech.intflix.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SeasonRepository extends CrudRepository<Season, Long> {
+}
---
+++ src/main/java/edu/intech/intflix/model/SeriesRepository.java
@@ -0,0 +1,6 @@
+package edu.intech.intflix.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SeriesRepository extends CrudRepository<Series, Long> {
+}
