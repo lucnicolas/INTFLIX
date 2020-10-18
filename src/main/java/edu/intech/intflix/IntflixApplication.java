5811a338ca69e232f2e67bfbcf8625c757b304d2
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-18 13:58:53 +0200

    Initialisation Spring Boot project

---
+++ src/main/java/edu/intech/intflix/IntflixApplication.java
@@ -0,0 +1,13 @@
+package edu.intech.intflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/intech/intflix/IntflixApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.intech.intflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
