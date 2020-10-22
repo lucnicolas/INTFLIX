56d6695d750e15def4c1e55fb1fdc52edc6d2334
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-22 17:19:27 +0200

    Adding season-list and season-from components

---
+++ src/main/ui/src/app/components/admin/season-form/season-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-season-form',
+  templateUrl: './season-form.component.html',
+  styleUrls: ['./season-form.component.sass']
+})
+export class SeasonFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
