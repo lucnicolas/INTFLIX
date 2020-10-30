724964e3c58634e4ac73817ca26e38a5a5219cf5
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-30 16:30:37 +0100

    Adding fontawesome and ng-bootstrap dependencies

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,15 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'; // CLI imports router
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth/auth.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SeriesListComponent } from './components/admin/series-list/series-list.component';
 import { SeriesFormComponent } from './components/admin/series-form/series-form.component';
-import {AppRoutingModule} from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
 import { SeasonListComponent } from './components/admin/season-list/season-list.component';
 import { SeasonFormComponent } from './components/admin/season-form/season-form.component';
 import { EpisodeListComponent } from './components/admin/episode-list/episode-list.component';
@@ -31,12 +32,14 @@ import { AdminHomeComponent } from './components/admin/admin-home/admin-home.com
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
   ],
   exports: [RouterModule],
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
