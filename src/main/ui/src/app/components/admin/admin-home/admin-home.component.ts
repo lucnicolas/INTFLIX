d4d34042141b5945b5277fb7fa72e1bb77533287
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-30 16:32:49 +0100

    Updating admin home

--- src/main/ui/src/app/components/admin/admin-home/admin-home.component.ts
+++ src/main/ui/src/app/components/admin/admin-home/admin-home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {SeriesService} from "../../../service/series/series.service";
+import {Series} from "../../../model/series/series";
 
 @Component({
   selector: 'app-admin-home',
@@ -8,25 +11,62 @@ import {Router} from '@angular/router';
 })
 export class AdminHomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  selectedSeries: Series;
+  seriesList: Series[];
+  showDetails: boolean;
+  closeResult: string;
+
+  constructor(private router: Router,
+              private modalService: NgbModal,
+              private seriesService: SeriesService) { }
 
   ngOnInit(): void {
+    this.getAllSeries();
   }
 
-  gotoSeriesList(): void {
-    this.router.navigate(['/admin/series']);
+  getAllSeries(): void {
+    this.seriesService.findAll().subscribe(data => { this.seriesList = data });
   }
 
-  gotoSeasonList(): void {
-    this.router.navigate(['/admin/season']);
+  /**
+   * Open a series modal
+   * @param content Modal content
+   * @param series If unset create a new series else allow to edit it.
+   * @param showDetails If true disable all fields
+   */
+  open(content: any, series?: Series, showDetails?: boolean) {
+
+    this.showDetails = (showDetails) ? this.showDetails = true : false;
+    this.selectedSeries = (series != null) ? series : new Series();
+
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title',})
+      .result
+      .then(
+        (result) => { this.closeResult = `Closed with: ${result}`; },
+        (reason) => { this.closeResult = `Dismissed ${AdminHomeComponent.getDismissReason(reason)}`;} )
+      .then(() => this.getAllSeries());
   }
 
-  gotoEpisodeList(): void {
-    this.router.navigate(['/admin/episode']);
+  private static getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
-  gotoUserList(): void {
-    // this.router.navigate(['/admin/user']);
+  onSubmit(): void {
+    this.seriesService.save(this.selectedSeries).subscribe(() => {
+      this.getAllSeries();
+      this.modalService.dismissAll();
+    });
+  }
+
+  onDelete(series: Series): void {
+    this.seriesService.delete(series).subscribe(() => this.getAllSeries());
   }
 
 }
